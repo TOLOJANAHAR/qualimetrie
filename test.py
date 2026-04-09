6800019414ea9e32f73263bcd153e13e214e1c08
TOLOJANAHAR <tolojanaharynatachanoeline@gmail.com>
2026-04-09 14:18:14 +0300

    fix W292 line error

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ def test_business_price():
     data = {"base_price": 100, "class": "Business", "baggage": 0}
     assert calculate_final_price(data) == 300
 
+
 def test_baggage_25kg():
     data = {"base_price": 100, "class": "Economy", "baggage": 25}
     assert calculate_final_price(data) == 150
+
 
 def test_baggage_35kg():
     data = {"base_price": 100, "class": "Economy", "baggage": 35}
