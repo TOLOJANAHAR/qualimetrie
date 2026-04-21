c8da30adf0d85311831febe412b2aae4ccdb6bf7
TOLOJANAHAR <tolojanaharynatachanoeline@gmail.com>
2026-04-21 20:01:28 +0300

    fix W292

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from main import compute_final_price
 
+
 def test_business():
     assert compute_final_price({
         "base_price": 100,
@@ -7,11 +8,13 @@ def test_business():
         "baggage": 0
     }) == 300
 
+
 def test_baggage_25():
     assert compute_final_price({
         "base_price": 100,
         "baggage": 25
     }) == 150
+
 
 def test_baggage_35():
     assert compute_final_price({
